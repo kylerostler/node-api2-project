68df149639bb690f69d478285577e033db67faa6
kylerostler <93017344+kylerostler@users.noreply.github.com>
2022-03-08 15:31:31 -0700

    building post

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -19,17 +19,42 @@ router.get('/', (req, res) => {
 })
 router.get('/:id', async (req, res) => {
     try{
-        throw new Error('pathetic')
+        const post = await Post.findById(req.params.id)
+        if (!post) {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist"
+            })
+        }else {
+            res.json(post)
+        }
     } catch (err) {
         res.status(500).json({
-            message: "The post with the specified ID does not exist",
+            message: "The post information could not be retrieved",
             err: err.message,
             stack: err.stack,
         })
     }
 })
 router.post('/', (req, res) => {
-
+    const { title, contents } = req.body
+    if(!title || !contents) {
+        res.status(400).json({
+            message: 'Please provide title and contents for the post'
+        })
+    } else {
+        Post.insert({ title, contents })
+        .then(({ id }) => {
+            return Post.findById(id)
+        })
+        .then(post => {
+            res.status(201).json(post)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "There was an error while saving the post to the database"
+            })
+        })
+    }
 })
 router.delete('/:id', (req, res) => {
 
